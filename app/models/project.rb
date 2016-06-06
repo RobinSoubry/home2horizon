8d0c36c5a3f379955b5750018635e2197d07e391
Robin Soubry <robin.soubry@gmail.com>
2016-06-06 09:59:35 -0700

    Add validations on projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ActiveRecord::Base
-  # Remember to create a migration!
+  # Validations
+  validates :project_name, presence: true
+  validate :has_users?
+
+    def has_users?
+      errors.add(:users, "A project must have at least one user.") if self.users.blank?
+    end
+
+  # Associations
   has_and_belongs_to_many :users
   has_many :likes, :as => :likeable
   has_many :requests
