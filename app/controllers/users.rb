a29acf0c71ccb1b53d6ad0940cf14a9ee63e7537
Robin Soubry <robin.soubry@gmail.com>
2016-06-05 23:01:37 -0700

    Add route for login of users

---
+++ app/controllers/users.rb
@@ -0,0 +1,11 @@
+post '/login' do
+    @user = User.find_by_email(params[:email])
+  if @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect "/"
+  else
+    redirect '/fail'
+  end
+end
+
+
