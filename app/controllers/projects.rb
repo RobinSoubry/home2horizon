85be777842033987642dfe3c0dc54a198b78685c
Robin Soubry <robin.soubry@gmail.com>
2016-06-06 15:43:06 -0700

    Add feature for creating new project with Google geolocalization

--- app/controllers/projects.rb
+++ app/controllers/projects.rb
@@ -3,7 +3,24 @@ get '/projects' do
   erb :'projects/explore'
 end
 
+get '/projects/new' do
+  erb :'projects/new_project'
+end
+
+post '/projects' do
+  p '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+  @project = Project.new(params[:project])
+  @project.users << User.find(session[:user_id])
+  tags = params[:tags]
+  @project.tags = tags.split(" ")
+  @project.save
+  p '#################################'
+  p @project.tags
+  p '#################################'
+  redirect "/projects/#{@project.id}"
+end
+
 get '/projects/:id' do
-  @project = Project.find(paramd[:id])
+  @project = Project.find(params[:id])
   erb :'projects/detail'
 end
