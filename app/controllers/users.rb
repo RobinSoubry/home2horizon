3345123d02aec1692a7c7cb9dcfb716795e27cea
Robin Soubry <robin.soubry@gmail.com>
2016-06-05 23:02:41 -0700

    Add route for creating new users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -12,3 +12,20 @@ get '/register' do
   erb :'users/register'
 end
 
+def create
+  @user = User.new(params[:user])
+  @user.password = params[:password]
+  @user.save!
+end
+
+post '/users' do
+  if create
+      # After registration the user automatically gets access his views
+    @user = User.find_by_email(params[:user][:email])
+    session[:user_id] = @user.id
+    redirect "/profile"
+  else
+    erb :'users/register'
+  end
+end
+
