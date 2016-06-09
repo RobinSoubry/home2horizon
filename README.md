5d94ba8476b2bf522a3a336e4d85ac3111ed857d
Robin Soubry <robin.soubry@gmail.com>
2016-06-09 12:20:23 -0700

    Add dynamic Google Maps plotting on map

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # home2horizon
 Passion Project DBC
+
+### To Do's ##
+* store x,y coordinates instead of calling GeoCode API
--- app/controllers/projects.rb
+++ app/controllers/projects.rb
@@ -1,6 +1,6 @@
 get '/projects' do
   @projects = Project.all.shuffle
-  erb :'projects/explore'
+  erb :'projects/explore', layout: :map_layout
 end
 
 get '/projects/new' do
@@ -37,4 +37,16 @@ get '/projects/:id' do
   @project = Project.find(params[:id])
   @members = User.find(@project.users.ids)
   erb :'projects/detail'
+end
+
+get '/locations' do
+  if request.xhr?
+    @projects = Project.all
+    @locations = @projects.map(&:location)
+    p @locations
+    content_type :json
+      { :locations => @locations }.to_json
+  else
+    redirect '/projects'
+  end
 end
